3190f7006b590b4aebaa7bb44c8c137d62027449
Suobig <apopovwork@gmail.com>
2019-08-08 16:55:53 +0300

    (fix) Make address display properly; (refactor) simplify object creation

--- React/projects/ravenous/src/util/Yelp.js
+++ React/projects/ravenous/src/util/Yelp.js
@@ -11,18 +11,25 @@ const Yelp = {
     const convertResponse = response => response.json();
     const parseRespose = json => {
       if (json.businesses) {
-        return json.businesses.map(b => ({
-          id: b.id,
-          imageSrc: b.image_url,
-          name: b.name,
-          address: `${b.location.address1} ${b.location.address2} ${b.location.address3}`.trim(),
-          city: b.location.city,
-          state: b.location.state,
-          zipCode: b.location.zip_code,
-          category: b.categories.map(item => item.title).join(', '),
-          rating: b.rating,
-          reviewCount: b.review_count
-        }));
+        return json.businesses.map(b => {
+          const address1 = b.location.address1 ? b.location.address1 : "";
+          const address2 = b.location.address2 ? b.location.address2 : "";
+          const address3 = b.location.address3 ? b.location.address3 : "";
+          const address = `${address1} ${address2} ${address3}`.trim();
+          const categories = b.categories.map(item => item.title).join(', ');
+          return {
+            id: b.id,
+            imageSrc: b.image_url,
+            name: b.name,
+            address: address,
+            city: b.location.city,
+            state: b.location.state,
+            zipCode: b.location.zip_code,
+            category: categories,
+            rating: b.rating,
+            reviewCount: b.review_count
+          }
+        });
       } else {
         return [];
       }
