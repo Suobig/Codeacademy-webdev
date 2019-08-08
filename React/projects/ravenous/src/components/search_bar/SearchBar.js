39d1167d526edc39d59c6cbfb1032c07b360dc34
Suobig <apopovwork@gmail.com>
2019-08-08 11:50:14 +0300

    (feature) Add state to SearchBar; add event handling for SortByOption

--- React/projects/ravenous/src/components/search_bar/SearchBar.js
+++ React/projects/ravenous/src/components/search_bar/SearchBar.js
@@ -1,29 +1,51 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import "./SearchBar.css";
 
-const sortByOptions = {
-  "Best Match": "best_match",
-  "Highest Rated": "rating",
-  "Most Reviewed": "review_count",
-}
-
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.sortByOptions = {
+      "Best Match": "best_match",
+      "Highest Rated": "rating",
+      "Most Reviewed": "review_count"
+    };
+    this.state = {
+      term: "",
+      location: "",
+      sortBy: this.sortByOptions["Best Match"]
+    };
+  }
+
+  getSortByClass(sortByOption) {
+    // console.log({ arg: sortByOption, state: this.state.sortBy});
+    
+    return this.state.sortBy === sortByOption ? "active" : "";
+  }
+
+  handleSortByClick(sortByOptionValue) {    
+    this.setState({ sortBy: sortByOptionValue });
+  }
+
   renderSortByOption() {
-    return (
-      Object.keys(sortByOptions).map(option => {
-        const apiKey = sortByOptions[option];
-        return <li key={apiKey}>{option}</li>
-      })
-    );
+    return Object.keys(this.sortByOptions).map(sortByOptionKey => {
+      const sortByOptionValue = this.sortByOptions[sortByOptionKey];
+      return (
+        <li 
+          key={sortByOptionValue}
+          className={this.getSortByClass(sortByOptionValue)}
+          onClick={this.handleSortByClick.bind(this, sortByOptionValue)}
+        >
+          {sortByOptionKey}
+        </li>
+      );
+    });
   }
 
   render() {
     return (
       <div className="SearchBar">
         <div className="SearchBar-sort-options">
-          <ul>
-            {this.renderSortByOption()}
-          </ul>
+          <ul>{this.renderSortByOption()}</ul>
         </div>
         <div className="SearchBar-fields">
           <input placeholder="Search Businesses" />
